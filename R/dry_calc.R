7084f8be41f583d5f7348f44cba77e0b30ea251f
Marian Agyby <marian9@student.ubc.ca>
2023-01-21 17:21:04 -0800

    fixed typo

--- R/dry_calc.R
+++ R/dry_calc.R
@@ -1,7 +1,7 @@
 #' @title dry_calc
-#' @description Calculates probability of at least one occurence of an event given the number of attempts.
+#' @description Calculates probability of at least one occurrence of an event given the number of attempts.
 #'
-#' @param p Probability of event occurence.
+#' @param p Probability of event occurrence.
 #' @param n The number of attempts.
 #' @param verbose Controls format of returned probability. Default (TRUE) returns result as a chr statement; FALSE returns result as a numeric.
 #' @param plot Controls return of plot showing where the resulting probability lies on the binomial distribution. Default (TRUE) shows the plot.
@@ -14,6 +14,7 @@
 #' dry_calc(0.2, 5, verbose = FALSE, plot = TRUE)
 #' # return probability in a chr statement and don't show plot
 #' dry_calc(0.2, 5, verbose = TRUE, plot = FALSE)
+#'
 dry_calc <- function(p, n, verbose = TRUE, plot = TRUE){
   NULL
 }
