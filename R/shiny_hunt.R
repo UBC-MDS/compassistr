390057ef454b487419fd339e2b113ffdc12d57b6
waielonh <waielonh@student.ubc.ca>
2023-01-23 18:50:22 -0800

    Updated shiny_hunt docstring

--- R/shiny_hunt.R
+++ R/shiny_hunt.R
@@ -1,17 +1,20 @@
 #' Calculates number of attempts/hrs to encounter/hatch a shiny
 #'
 #' @param gen Integer denoting generation of pokemon
-#' @param masuda Is the player using masuda method
-#' @param shiny_charm Does the player have a shiny charm
-#' @param encounter_rate Rate of encounter of the pokemon (only for wild encounters)
-#' @param atempt_time Time (in seconds) representing average time taken to encounter a pokemon, or soft reset
-#' @param hatch_time Time (in seconds) to hatch a single pokemon egg
-#' @param verbose Controls format of returned probability. Default (True) prints results as statements, False returns a dict.
+#' @param masuda Is the player using masuda method. Default is FALSE
+#' @param shiny_charm Does the player have a shiny charm. Default is FALSE
+#' @param encounter_rate Rate of encounter of the pokemon (only for wild encounters). Default is 100%
+#' @param atempt_time Time (in seconds) representing average time taken to encounter a pokemon, or soft reset. Default value is 15 seconds per encounter
+#' @param hatch_time Time (in seconds) to hatch a single pokemon egg. Default value is 0
 #'
-#' @return Dictionary containing probabilities as keys and number of attempts/hrs values as tuples
+#' @return Tibble containing probabilities, number of attempts, and hours
 #' @export
 #'
 #' @examples
+#' gen = 8
+#' e_rate = 75
+#' att = 20
+#' shiny_hunt(gen = gen, encounter_rate = e_rate, attempt_time = att, shiny_charm = TRUE)
 shiny_hunt <- function(gen, masuda = FALSE, shiny_charm = FALSE, encounter_rate = 100, attempt_time = 15, hatch_time = 0) {
 
   # making sure all inputs are legal
