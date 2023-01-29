c0b9a0db1048b03a7adbb8f2bff24a59ced1efd9
Ashwin Babu <ash2507@student.ubc.ca>
2023-01-28 16:27:30 -0800

    Changed docstrings for the pts-calc function

--- R/pts_calc.R
+++ R/pts_calc.R
@@ -1,14 +1,17 @@
 #' Calculates and returns the list of time required (in ranked order) to achieve target points using the different options provided in input 
 #'
-#' @param points_attempt list of number of points obtained in each attempt
-#' @param time_attempt list of time taken (in minutes) for each attempt
-#' @param target_points number of points targetted to reach
-#' @param verbose bool Controls format of returned time taken. Default (True) prints results as statements, False returns a list.
+#' @param points_attempt numeric vector for number of points obtained in each attempt
+#' @param time_attempt numeric vector for time taken (in minutes) for each attempt
+#' @param target_points Float / Integer number of points targetted to reach
 #'
-#' @return list of time required (in ranked order of minutes) to achieve target points using the different options provided in input
+#' @return numeric vector for time required (in ranked order of minutes) to achieve target points using the different options provided in input
 #' @export
 #'
 #' @examples
+#' points_attempt = c(100,20,120,150,200,30)
+#' time_attempt = c(2,3,2,5,6,2)
+#' target_points = 200
+#' pts_calc(points_attempt = points_attempt,time_attempt = time_attempt ,target_points = target_points)
 pts_calc <- function(points_attempt, time_attempt,  target_points, verbose=TRUE){
   print(points_attempt)
 }
